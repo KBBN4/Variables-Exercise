d1651fcf293f1673263fbd0f61215be9a0f4ead0
katherine Buendia <kbbuendia24@gmail.com>
2023-04-07 14:32:12 -0400

    Variables Story

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string pet = "Lex";
+            int age = 10;
+            char name ='l';
+            bool Story = true;
+            double distance = 3.5;
+            decimal cost = 10.99m;
+            Console.WriteLine($" I had a dog named {pet}.He was about {age} years old when he passed.We used {name} to call him.Think this story is true? well, it is {Story}. " +
+                $"We used to walk often, about {distance} steps a day.Ha!. If you like this story purchase for {cost}. " +
+                $"Thank you!" );
+
+             
+
         }
     }
 }
